74b99516b3c8ef78ca89bda82a7b9a640c3b8ad4
Huzaifa-Emumba <huzaifa.arshad@emumba.com>
2025-03-30 00:20:09 +0500

    Init auth flow

---
+++ app/services/auth/auth_service.py
@@ -0,0 +1,6 @@
+class AuthService:
+    def __init__(self):
+        pass
+
+    def login(self, email, password):
+        pass
---
+++ app/services/users/users_service.py
@@ -0,0 +1,12 @@
+class UserService:
+    def __init__(self):
+        pass
+
+    def create_user(self, userMeta):
+        pass
+
+    def update_user(self, userMeta):
+        pass
+
+    def delete_user(self, id):
+        pass
